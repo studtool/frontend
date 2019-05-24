62b5c8e44b1f9ece098e65eac0355f2f6e8e1f87
nikitarub <nikita.rub@yandex.ru>
2019-05-24 08:24:30 +0300

    Added simple file system viewer

--- src/app/components/organisms/fileSystemViewer/fileSystemViewer.js
+++ src/app/components/organisms/fileSystemViewer/fileSystemViewer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Button} from '../../atoms/button/button.js';
-import {InputText} from '../../molecules/inputText/inputText.js';
+
+import {FileSystemContainer} from '../../molecules/fileSystemContainer/fileSystemContainer';
+import {PlusContainer} from '../../molecules/plusContainer/plusContainer';
 
 export class FileSystemViewer extends Component {
     constructor(props) {
@@ -14,9 +15,40 @@ export class FileSystemViewer extends Component {
             handleSubmit = null,
         } = this.props;
 
+
+        const folderItems = (data) => {
+            console.log("data: ",data);
+            
+            const foldersRes = data.map((folders) => 
+                <>
+                    <PlusContainer>
+                        <FileSystemContainer>
+                            {folders.folder}
+                        </FileSystemContainer>
+                    </PlusContainer>
+                </>
+            );
+            return foldersRes;
+        }
+
+        const dataStub = [
+            {
+                folder: 'hi',
+                files: ['hi file 1', 'hi file 2']
+            },
+            {
+                folder: 'lol',
+                files: ['lol file 1', 'lol file 2']
+            },
+            {
+                folder: 'kek',
+                files: ['kek file 1', 'kek file 2']
+            },
+        ]
+
         return (
             <>
-                lol
+                {folderItems(dataStub)}
             </>
         );
     };
